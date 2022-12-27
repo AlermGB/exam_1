74727825cfbc3ef08dcc5593ecbd84ee8a6a18ad
Alexandr <kaperisatiom@mail.ru>
2022-12-28 02:01:35 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] mainArray={"hello","2","world",":-)"};
+// string[] mainArray={"1234","1567","-2","computer scince"};
+// string[] mainArray={"Russia","Denmark","Kazan"};
+int checkLength=3;
+int resultArrayLength=0;
+int j=0;
+
+for(int i=0;i<mainArray.Length;i++){
+    if(mainArray[i].Length<=checkLength){
+        resultArrayLength++;
+    }
+}
+string[] resultArray = new string[resultArrayLength];
+
+for(int i=0;i<mainArray.Length;i++){
+    if(mainArray[i].Length<=checkLength){
+        resultArray[j]=mainArray[i];
+        j++;
+    }
+}
+Console.WriteLine(string.Join(" ", resultArray));
